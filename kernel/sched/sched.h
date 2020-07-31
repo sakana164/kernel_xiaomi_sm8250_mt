ceeeacb523af2022f47491e0d3db77f86087e2b2
Valentin Schneider <valentin.schneider@arm.com>
2020-07-31 20:20:14 +0100

    sched: Document arch_scale_*_capacity()
    Rather that hide their purpose in some dark, damp corner of Documentation/,
    add some documentation to the default implementations.
    
    Signed-off-by: Valentin Schneider <valentin.schneider@arm.com>
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Link: https://lore.kernel.org/r/20200731192016.7484-2-valentin.schneider@arm.com
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2317,6 +2317,16 @@ static inline u64 sched_ktime_clock(void)
 }
 
 #ifndef arch_scale_freq_capacity
+/**
+ * arch_scale_freq_capacity - get the frequency scale factor of a given CPU.
+ * @cpu: the CPU in question.
+ *
+ * Return: the frequency scale factor normalized against SCHED_CAPACITY_SCALE, i.e.
+ *
+ *     f_curr
+ *     ------ * SCHED_CAPACITY_SCALE
+ *     f_max
+ */
 static __always_inline
 unsigned long arch_scale_freq_capacity(int cpu)
 {
