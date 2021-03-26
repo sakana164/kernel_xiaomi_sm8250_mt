0386322ad502a5c7916cca17f31674b02ffbb360
Peter Zijlstra <peterz@infradead.org>
2021-03-26 18:55:06 +0100

    sched: Trivial core scheduling cookie management
    In order to not have to use pid_struct, create a new, smaller,
    structure to manage task cookies for core scheduling.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Tested-by: Hongyu Ning <hongyu.ning@linux.intel.com>
    Tested-by: Vincent Guittot <vincent.guittot@linaro.org>
    Link: https://lkml.kernel.org/r/20210422123308.919768100@infradead.org
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1325,6 +1325,22 @@ bool cfs_prio_less(struct task_struct *a, struct task_struct *b, bool fi);
 
 extern void queue_core_balance(struct rq *rq);
 
+static inline bool sched_core_enqueued(struct task_struct *p)
+{
+	return !RB_EMPTY_NODE(&p->core_node);
+}
+
+extern void sched_core_enqueue(struct rq *rq, struct task_struct *p);
+extern void sched_core_dequeue(struct rq *rq, struct task_struct *p);
+
+extern void sched_core_get(void);
+extern void sched_core_put(void);
+
+extern unsigned long sched_core_alloc_cookie(void);
+extern void sched_core_put_cookie(unsigned long cookie);
+extern unsigned long sched_core_get_cookie(unsigned long cookie);
+extern unsigned long sched_core_update_cookie(struct task_struct *p, unsigned long cookie);
+
 #else /* !CONFIG_SCHED_CORE */
 
 static inline bool sched_core_enabled(struct rq *rq)
