6c6e24e78ee3a977453e54ca298df4ab6b3c459a
utziacre <utziacre@gmail.com>
2023-11-21 23:40:45 +0800

    sched/cass: Choose a better CPU for important tasks
    Change-Id: I26f58f774b8bc9b9391c66ce917d834ee858675c

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -87,11 +87,14 @@ bool cass_prime_cpu(const struct cass_cpu_cand *c)
 static __always_inline
 bool cass_cpu_better(const struct cass_cpu_cand *a,
 		     const struct cass_cpu_cand *b,
-		     int this_cpu, int prev_cpu)
+		     int this_cpu, int prev_cpu, struct task_struct *p)
 {
 #define cass_cmp(a, b) ({ res = (a) - (b); })
 #define cass_eq(a, b) ({ res = (a) == (b); })
 	long res;
+	bool low_util = (a->util <= sched_util_threshold[a->cpu] &&
+            		 b->util <= sched_util_threshold[b->cpu]);
+	bool boosted = uclamp_boosted(p) && (p->prio <= DEFAULT_PRIO - 10);
 
 	/* Prefer the CPU that's not overloaded */
 	if (cass_cmp(b->eff_util / b->cap_max, a->eff_util / a->cap_max))
@@ -102,12 +105,11 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	    cass_cmp(b->eff_util * SCHED_CAPACITY_SCALE / b->cap_max,
 		     a->eff_util * SCHED_CAPACITY_SCALE / a->cap_max))
 		goto done;
-	
+
 	/* Prefer the CPU with lower orig capacity when util is low */
-	if((a->util <= sched_util_threshold[a->cpu] &&
-	    b->util <= sched_util_threshold[b->cpu]) &&
-	    cass_cmp(capacity_orig_of(b->cpu), capacity_orig_of(a->cpu)))
-                goto done;
+	if(low_util && !boosted && cass_cmp(capacity_orig_of(b->cpu),
+					    capacity_orig_of(a->cpu)))
+        goto done;
 
 	/* Prefer the CPU that isn't the single fastest one in the system */
 	if (cass_cmp(cass_prime_cpu(b), cass_prime_cpu(a)))
@@ -264,7 +266,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool rt)
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
 		if (best == curr ||
-		    cass_cpu_better(curr, best, this_cpu, prev_cpu)) {
+		    cass_cpu_better(curr, best, this_cpu, prev_cpu, p)) {
 			best = curr;
 			cidx ^= 1;
 		}
