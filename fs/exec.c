19c20f90b81d466d17a426e73e8d5c0b85d75827
simonpunk2016 <simonpunk2016@gmail.com>
2024-04-04 02:56:51 +0800

    susfs: Bump version to 1.5.2
    Signed-off-by: Rudi Setiyawan <diphons@gmail.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1955,7 +1955,11 @@ out_ret:
 }
 
 #ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+extern bool susfs_is_sus_su_hooks_enabled __read_mostly;
+#endif
 extern bool ksu_execveat_hook __read_mostly;
+
 extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *argv,
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr,
@@ -1966,12 +1970,16 @@ static int do_execveat_common(int fd, struct filename *filename,
 			      struct user_arg_ptr envp,
 			      int flags)
 {
-	#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+	if (unlikely(ksu_execveat_hook) || susfs_is_sus_su_hooks_enabled)
+#else
 	if (unlikely(ksu_execveat_hook))
+#endif
 		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	else
 		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
-    #endif
+#endif
 	return __do_execve_file(fd, filename, argv, envp, flags, NULL);
 }
 
