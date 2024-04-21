f40593b6d8883efbbd0e0ebd133f7b3f70c02c92
Juansheng <jaycopter@foxmail.com>
2024-04-21 18:09:40 +0800

    sched/core: Remove uclamp_task() and make task_util_est() static
    uclamp_task() is not used anymore. Remove it so that task_util_est() can
    be static.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4193,7 +4193,7 @@ static inline unsigned long _task_util_est(struct task_struct *p)
 	return READ_ONCE(p->se.avg.util_est) & ~UTIL_AVG_UNCHANGED;
 }
 
-unsigned long task_util_est(struct task_struct *p)
+static inline unsigned long task_util_est(struct task_struct *p)
 {
 	return max(task_util(p), _task_util_est(p));
 }
@@ -4216,8 +4216,6 @@ static inline void util_est_enqueue(struct cfs_rq *cfs_rq,
 	trace_sched_util_est_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 }
 
-#define UTIL_EST_MARGIN (SCHED_CAPACITY_SCALE / 100)
-
 static inline void util_est_dequeue(struct cfs_rq *cfs_rq,
 				    struct task_struct *p)
 {
@@ -4234,6 +4232,8 @@ static inline void util_est_dequeue(struct cfs_rq *cfs_rq,
 	/* Update plots for CPU's estimated utilization */
 	trace_sched_util_est_cpu(cpu_of(rq_of(cfs_rq)), cfs_rq);
 }
+
+#define UTIL_EST_MARGIN (SCHED_CAPACITY_SCALE / 100)
 
 static inline void util_est_update(struct cfs_rq *cfs_rq,
 				   struct task_struct *p,
@@ -6727,6 +6727,7 @@ cpu_util(int cpu, struct task_struct *p, int dst_cpu, int boost)
 
 	if (sched_feat(UTIL_EST)) {
 		unsigned long util_est;
+
 		util_est = READ_ONCE(cfs_rq->avg.util_est);
 
 		/*
