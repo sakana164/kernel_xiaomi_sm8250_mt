ff9b59184ba51250147ec12c51e17d1c3e1dd619
Juansheng <jaycopter@foxmail.com>
2024-06-09 17:11:11 +0800

    sched/fair: Fix load balance on big.LITTLE platform
    Revert "sched/fair: Use all little CPUs for CPU-bound workloads"
    This reverts commit 8685502b47c93ea976452cbc051787339c1fbe1b.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8179,7 +8179,7 @@ static int detach_tasks(struct lb_env *env)
 		case migrate_util:
 			util = task_util_est(p);
 
-			if (shr_bound(util, env->sd->nr_balance_failed) > env->imbalance)
+			if (util > env->imbalance)
 				goto next;
 
 			env->imbalance -= util;
