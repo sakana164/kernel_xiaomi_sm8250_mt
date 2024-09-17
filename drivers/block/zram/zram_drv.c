d2c117ae048888cedfdc58dff54b43c3308cf7ff
Sergey Senozhatsky <senozhatsky@chromium.org>
2024-09-17 11:09:07 +0900

    zram: permit only one post-processing operation at a time
    Both recompress and writeback soon will unlock slots during processing,
    which makes things too complex wrt possible race-conditions.  We still
    want to clear PP_SLOT in slot_free, because this is how we figure out that
    slot that was selected for post-processing has been released under us and
    when we start post-processing we check if slot still has PP_SLOT set.  At
    the same time, theoretically, we can have something like this:
    
    CPU0			    CPU1
    
    recompress
    scan slots
    set PP_SLOT
    unlock slot
    			slot_free
    			clear PP_SLOT
    
    			allocate PP_SLOT
    			writeback
    			scan slots
    			set PP_SLOT
    			unlock slot
    select PP-slot
    test PP_SLOT
    
    So recompress will not detect that slot has been re-used and re-selected
    for concurrent writeback post-processing.
    
    Make sure that we only permit on post-processing operation at a time.  So
    now recompress and writeback post-processing don't race against each
    other, we only need to handle slot re-use (slot_free and write), which is
    handled individually by each pp operation.
    
    Having recompress and writeback competing for the same slots is not
    exactly good anyway (can't imagine anyone doing that).
    
    Link: https://lkml.kernel.org/r/20240917021020.883356-3-senozhatsky@chromium.org
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Cc: Minchan Kim <minchan@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -592,6 +592,12 @@ static ssize_t writeback_store(struct device *dev,
 		goto release_init_lock;
 	}
 
+	/* Do not permit concurrent post-processing actions. */
+	if (atomic_xchg(&zram->pp_in_progress, 1)) {
+		up_read(&zram->init_lock);
+		return -EAGAIN;
+	}
+
 	if (!zram->backing_dev) {
 		ret = -ENODEV;
 		goto release_init_lock;
@@ -716,6 +722,7 @@ next:
 		free_block_bdev(zram, blk_idx);
 	__free_page(page);
 release_init_lock:
+	atomic_set(&zram->pp_in_progress, 0);
 	up_read(&zram->init_lock);
 
 	return ret;
@@ -1882,6 +1889,12 @@ static ssize_t recompress_store(struct device *dev,
 		goto release_init_lock;
 	}
 
+	/* Do not permit concurrent post-processing actions. */
+	if (atomic_xchg(&zram->pp_in_progress, 1)) {
+		up_read(&zram->init_lock);
+		return -EAGAIN;
+	}
+
 	if (algo) {
 		bool found = false;
 
@@ -1949,6 +1962,7 @@ next:
 	__free_page(page);
 
 release_init_lock:
+	atomic_set(&zram->pp_in_progress, 0);
 	up_read(&zram->init_lock);
 	return ret;
 }
@@ -2157,6 +2171,7 @@ static void zram_reset_device(struct zram *zram)
 	zram->disksize = 0;
 	zram_destroy_comps(zram);
 	memset(&zram->stats, 0, sizeof(zram->stats));
+	atomic_set(&zram->pp_in_progress, 0);
 	reset_bdev(zram);
 
 	comp_algorithm_set(zram, ZRAM_PRIMARY_COMP, default_compressor);
@@ -2388,6 +2403,7 @@ static int zram_add(void)
 	zram->disk->queue->queuedata = zram;
 	zram->disk->private_data = zram;
 	snprintf(zram->disk->disk_name, 16, "zram%d", device_id);
+	atomic_set(&zram->pp_in_progress, 0);
 
 	/* Actual capacity set using sysfs (/sys/block/zram<id>/disksize */
 	set_capacity(zram->disk, 0);
