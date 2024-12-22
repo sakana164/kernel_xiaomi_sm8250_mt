f0bcac9fce9bffa696ddae4d1186ffbd5f2aef2b
TIMISONG-dev <dima.pilaer@gmail.com>
2024-12-22 20:42:26 +0300

    build: Switch to ZyC clang20

--- build.sh
+++ build.sh
@@ -20,7 +20,7 @@ git log $LAST..HEAD > ../changelog.txt
 BRANCH=$(git branch --show-current)
 
 # Каталоги компиляторов
-CLANG19_DIR=$KERNEL_DIR/clang19
+CLANG_DIR=$KERNEL_DIR/clang20
 ANDROID_PREBUILTS_GCC_ARM_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 ANDROID_PREBUILTS_GCC_AARCH64_DIR=$KERNEL_DIR/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
@@ -44,19 +44,19 @@ check_and_wget() {
         mkdir $dir
         cd $dir
         wget $repo
-        tar -zxvf Clang-19.0.0git-20240625.tar.gz
-        rm -rf Clang-19.0.0git-20240625.tar.gz
+        tar -zxvf Clang-20.0.0git-20241222.tar.gz
+        rm -rf Clang-20.0.0git-20241222.tar.gz
         cd ../kernel_xiaomi_sm8250
     fi
 }
 
 # Клонирование инструментов компиляции, если они не существуют
-check_and_wget $CLANG19_DIR https://github.com/ZyCromerZ/Clang/releases/download/19.0.0git-20240625-release/Clang-19.0.0git-20240625.tar.gz
+check_and_wget $CLANG_DIR https://github.com/ZyCromerZ/Clang/releases/download/20.0.0git-20241222-release/Clang-20.0.0git-20241222.tar.gz
 check_and_clone $ANDROID_PREBUILTS_GCC_ARM_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9
 check_and_clone $ANDROID_PREBUILTS_GCC_AARCH64_DIR https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
 
 # Установка переменных PATH
-PATH=$CLANG19_DIR/bin:$ANDROID_PREBUILTS_GCC_AARCH64_DIR/bin:$ANDROID_PREBUILTS_GCC_ARM_DIR/bin:$PATH
+PATH=$CLANG_DIR/bin:$ANDROID_PREBUILTS_GCC_AARCH64_DIR/bin:$ANDROID_PREBUILTS_GCC_ARM_DIR/bin:$PATH
 export PATH
 export ARCH=arm64
 
