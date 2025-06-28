f0723a1293bdd4a51252dcfb4839c3426f1529d8
Vayruz Rafli <mikhailovnakowalski@gmail.com>
2025-06-28 08:28:23 +0000

    mm: drop redundant return value check from walk_page_range
    The return value of walk_page_range() is no longer used,
    so remove the variable 'ret' and the conditional break.
    
    Change-Id: Ia2b08cce389d8bc4f670805f63a153f29a22aeeb
    Signed-off-by: Vayruz Rafli <mikhailovnakowalski@gmail.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1891,7 +1891,6 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	unsigned long start = 0;
 	unsigned long end = 0;
 	struct reclaim_param rp;
-	int ret;
 
 	memset(buffer, 0, sizeof(buffer));
 	if (count > sizeof(buffer) - 1)
@@ -1968,11 +1967,9 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			ret = walk_page_range(max(vma->vm_start, start),
+			walk_page_range(max(vma->vm_start, start),
 					min(vma->vm_end, end),
 					&reclaim_walk);
-			if (ret)
-				break;
 			vma = vma->vm_next;
 		}
 	} else {
@@ -1987,10 +1984,8 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 				continue;
 
 			rp.vma = vma;
-			ret = walk_page_range(vma->vm_start, vma->vm_end,
+			walk_page_range(vma->vm_start, vma->vm_end,
 				&reclaim_walk);
-			if (ret)
-				break;
 		}
 	}
 
